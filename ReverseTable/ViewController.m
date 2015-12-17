850f9c1a87382b24f03ca5df0b398c7a26ef3a2e
Natalie Podrazik <npodrazik@gmail.com>
2015-12-16 20:54:09 -0500

    moving data shift to draw's delay

--- ReverseTable/ViewController.m
+++ ReverseTable/ViewController.m
@@ -106,10 +106,10 @@ static NSUInteger ValuesPerPage = 50;
     self.isRedrawing = YES;
     [self.spinner startAnimating];
     
-    // Prepare data.
-    [self loadPreviousPageData];
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        // Prepare data.
+        [self loadPreviousPageData];
         [self drawPreviousPage];
     });
 }
